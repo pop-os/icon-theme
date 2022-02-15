f5a2a95d0bc664f7aad4142bc2973f32ee266bac
Ian Santopietro <isantop@gmail.com>
2022-02-14 17:51:15 -0700

    fix(rendering): remove outdated scaled icons
    We used to have to provide icons at 2x scale to handle HiDPI screens.
    Since we're now doing SVG icons, we can eliminate all of these and
    save further space.

--- src/bitmaps/render-bitmaps-nu.py
+++ src/bitmaps/render-bitmaps-nu.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-#
-# Legal Stuff:
-#
+
+"""
 # This file is part of the Pop Icon Theme and is free software; you can 
 # redistribute it and/or modify it under  the terms of the GNU Lesser General
 # Public License as published by the Free Software Foundation; version 3.
@@ -13,48 +12,57 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, see <https://www.gnu.org/licenses/lgpl-3.0.txt>
-#
-#
-# Thanks to the GNOME icon developers for the original version of this script
+"""
 
 import os
 import sys
 import xml.sax
 import subprocess
 import argparse
+from pathlib import Path
 
-INKSCAPE = '/usr/bin/inkscape'
-MAINDIR = '../../Pop'
-SOURCES = ('actions', 'apps', 'categories', 'devices', 'emblems', 'logos', 'mimetypes', 'places', 'preferences', 'status')
+BINDIR = Path('/usr/bin')
+BASEDIR = Path('../../')
+THEMENAME:str = 'Pop'
+INKSCAPE = BINDIR / 'inkscape'
+THEMEDIR = BASEDIR / THEMENAME
+CONTEXTS = (
+	'actions',
+	'apps',
+	'categories',
+	'devices',
+	'emblems',
+	'logos',
+	'mimetypes',
+	'places',
+	'preferences',
+	'status',
+)
 
-# the resolution that non-hi-dpi icons are rendered at
-DPI_1_TO_1 = 96
-# DPI multipliers to render at
-DPIS = [1]
+DPI_DIRECT = 96
 
-inkscape_process = None
-
-def main(args, SRC):
-
-	def inkscape_render_rect(icon_file, rect, dpi, output_file):
-
+def main(args, src):
+	
+	def render_icon(src_file, rect, dpi, output_file) -> None:
 		cmd = [
-			INKSCAPE,
-			'-d', str(dpi), # export-dpi
-			'-i', rect, # export-id
-			'-o', output_file, # export-filename
-			icon_file # input file
+			INKSCAPE, 
+			'-d', str(dpi), # --export-dpi
+			'-i', rect, # --export-id
+			'-o', output_file, # --export-filename
+			src_file # input file
 		]
-		print(f'Rendering {output_file}')
-		subprocess.run(cmd)
 
-	class ContentHandler(xml.sax.ContentHandler):
+		print(f'...Rendering {output_file}')
+		subprocess.run(cmd)
+	
+	class IconHandler(xml.sax.ContentHandler):
 		ROOT = 0
 		SVG = 1
 		LAYER = 2
 		OTHER = 3
 		TEXT = 4
-		def __init__(self, path, force=False, filter=None):
+
+		def __init__(self, path, force=False, filter=None) -> None:
 			self.stack = [self.ROOT]
 			self.inside = [self.ROOT]
 			self.path = path
@@ -63,44 +71,57 @@ def main(args, SRC):
 			self.chars = ""
 			self.force = force
 			self.filter = filter
-
+		
 		def endDocument(self):
 			pass
 
-		def startElement(self, name, attrs):
+		def startElement(self, name, attrs) -> None:
+			# Putting these conditionals here for better cleanliness.
+			# Otherwise the conditionals later in the code are long and gross.
+			name_is_g:bool = name == 'g'
+			name_is_text:bool = name == 'text'
+			is_groupmode: bool = 'inkscape:groupmode' in attrs
+			is_label:bool = 'inkscape:label' in attrs
+			groupmode_is_layer:bool = attrs['inkscape:groupmode'] == 'layer'
+			layer_is_baseplate:bool = attrs['inkscape:label'].startswith('Baseplate')
+			is_context:bool = attrs['inkscape:label'] == 'context'
+			is_icon_name = attrs['inkscape:label'] == 'icon-name'
+			
 			if self.inside[-1] == self.ROOT:
-				if name == "svg":
+				if name == 'svg':
 					self.stack.append(self.SVG)
 					self.inside.append(self.SVG)
 					return
+			
 			elif self.inside[-1] == self.SVG:
-				if (name == "g" and ('inkscape:groupmode' in attrs) and ('inkscape:label' in attrs)
-				   and attrs['inkscape:groupmode'] == 'layer' and attrs['inkscape:label'].startswith('Baseplate')):
-					self.stack.append(self.LAYER)
-					self.inside.append(self.LAYER)
-					self.context = None
-					self.icon_name = None
-					self.rects = []
-					return
+				if name_is_g and is_groupmode and is_label:
+					if groupmode_is_layer and layer_is_baseplate:
+						self.stack.append(self.LAYER)
+						self.inside.append(self.LAYER)
+						self.context = None
+						self.icon_name = None
+						self.rects = []
+						return
+			
 			elif self.inside[-1] == self.LAYER:
-				if name == "text" and ('inkscape:label' in attrs) and attrs['inkscape:label'] == 'context':
+				if name_is_text and is_label and is_context:
 					self.stack.append(self.TEXT)
 					self.inside.append(self.TEXT)
 					self.text='context'
 					self.chars = ""
 					return
-				elif name == "text" and ('inkscape:label' in attrs) and attrs['inkscape:label'] == 'icon-name':
+				elif name_is_text and is_label and is_icon_name:
 					self.stack.append(self.TEXT)
 					self.inside.append(self.TEXT)
 					self.text='icon-name'
 					self.chars = ""
 					return
-				elif name == "rect":
+				
+				elif name == 'rect':
 					self.rects.append(attrs)
-
+			
 			self.stack.append(self.OTHER)
-
-
+		
 		def endElement(self, name):
 			stacked = self.stack.pop()
 			if self.inside[-1] == stacked:
@@ -132,7 +153,7 @@ def main(args, SRC):
 						if dpi_factor != 1:
 							size_str += "@%sx" % dpi_factor
 
-						dir = os.path.join(MAINDIR, size_str, self.context)
+						dir = os.path.join(THEMEDIR, size_str, self.context)
 						outfile = os.path.join(dir, self.icon_name+'.svg')
 						if not os.path.exists(dir):
 							os.makedirs(dir)
@@ -155,17 +176,16 @@ def main(args, SRC):
 		def characters(self, chars):
 			self.chars += chars.strip()
 
-
 	if not args.svg:
-		if not os.path.exists(MAINDIR):
-			os.mkdir(MAINDIR)
+		if not os.path.exists(THEMEDIR):
+			os.mkdir(THEMEDIR)
 		print ('')
 		print ('Rendering from SVGs in', SRC)
 		print ('')
 		for file in os.listdir(SRC):
 			if file[-4:] == '.svg':
 				file = os.path.join(SRC, file)
-				handler = ContentHandler(file)
+				handler = IconHandler(file)
 				xml.sax.parse(open(file), handler)
 		print ('')
 	else:
@@ -187,6 +207,6 @@ parser.add_argument('filter', type=str, nargs='?', metavar='FILTER',
 
 args = parser.parse_args()
 
-for source in SOURCES:
-	SRC = os.path.join('.', source)
+for ctx in CONTEXTS:
+	SRC = os.path.join('.', ctx)
 	main(args, SRC)
